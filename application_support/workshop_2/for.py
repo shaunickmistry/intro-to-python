ed03b58afcd48893de0ab417215a14f86af2e7c5
Shaunick Mistry <shaunickmistry@hotmail.co.uk>
2020-11-20 09:56:29 +0000

    Workshop 2 example scripts

---
+++ application_support/workshop_2/for.py
@@ -0,0 +1,7 @@
+def main():
+    for x in range(5):
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
---
+++ application_support/workshop_2/range.py
@@ -0,0 +1,10 @@
+def main():
+    start = int(input("Start: "))
+    stop = int(input("Stop: "))
+
+    for x in range(start, stop):
+        print(x)
+
+
+if __name__ == "__main__":
+    main()
